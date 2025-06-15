8069086b71c3749d6df4cb8652166a5ecd195ad7
Armin Rüegger <armin.rueegger@ergon.ch>
2025-06-15 15:07:39 +0200

    ausgangslage für addPower

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -1,5 +1,5 @@
-import '../css/mvp.css';
 import {useState} from "react";
+import '../css/mvp.css';
 import formelrad from "../image/formelradelektronik.gif";
 import InputField from "../formular/InputField";
 
@@ -9,7 +9,6 @@ export default function Formelrad() {
         i: 2,
         r: ""
     })
-
     return (
         <>
             <section>
@@ -19,7 +18,7 @@ export default function Formelrad() {
                 </header>
                 <form>
                     <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
-                    <InputField color={"black"} value={values.i} label="Stromstaerke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
+                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <button type="submit">Calculate</button>
                 </form>
